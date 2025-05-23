b3e5e7f1e45f0a40a7283fad24dd38437fbc9534
ledemkam <ledemkam@gmail.com>
2025-05-23 20:19:33 +0200

    implement  exception

---
+++ backend/src/main/java/com/ledemkam/backend/dto/ErrorDto.java
@@ -0,0 +1,12 @@
+package com.ledemkam.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private String error;
+}
