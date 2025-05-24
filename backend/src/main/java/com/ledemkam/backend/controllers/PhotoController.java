b449226a0ec48c8f68429b0a650142b80fe4b4e8
ledemkam <ledemkam@gmail.com>
2025-05-24 18:02:04 +0200

    photo_retrieval

--- backend/src/main/java/com/ledemkam/backend/controllers/PhotoController.java
+++ backend/src/main/java/com/ledemkam/backend/controllers/PhotoController.java
@@ -5,10 +5,12 @@ import com.ledemkam.backend.domain.entities.Photo;
 import com.ledemkam.backend.mappers.PhotoMapper;
 import com.ledemkam.backend.services.PhotoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -25,5 +27,17 @@ public class PhotoController {
             @RequestParam("file") MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping("/{id:.+}") //The {id:.+} path variable pattern allows for file extensions in the ID
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(MediaTypeFactory
+                                .getMediaType(photo)
+                                .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 }
