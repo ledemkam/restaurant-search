8778f3992d26d304a7f9673cf7f8c8cb7336bc11
ledemkam <ledemkam@gmail.com>
2025-05-25 17:32:41 +0200

    listReviews

--- backend/src/main/java/com/ledemkam/backend/services/ReviewService.java
+++ backend/src/main/java/com/ledemkam/backend/services/ReviewService.java
@@ -3,8 +3,11 @@ package com.ledemkam.backend.services;
 import com.ledemkam.backend.domain.entities.Review;
 import com.ledemkam.backend.domain.entities.User;
 import com.ledemkam.backend.domain.request.ReviewCreateUpdateRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
     Review createReview(User author, String restaurantId,
                         ReviewCreateUpdateRequest review);
+    Page<Review> listReviews(String restaurantId, Pageable pageable);
 }
